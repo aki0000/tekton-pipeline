dc3bedf452ff8e6d260eb8ec9e73347a7695d7b4
aki0000 <akihisa.fromm1@gmail.com>
2022-04-09 15:04:38 +0900

    Updated

---
+++ ingress/install.sh
@@ -0,0 +1,11 @@
+# Deploy ingress for tekton pipeline
+kubectl apply -f ingress.yaml
+
+# Apply hostname into your /etc/hosts
+if grep -q "tekton.dashboard.local" /etc/hosts;
+then
+    cat << EOF >> /etc/hosts
+    # tekton dashboard host
+    127.0.0.1 tekton.dashboard.local
+EOF;
+fi
