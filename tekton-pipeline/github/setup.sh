891ad0fe03961e83082c4521800f7f3b792fb017
aki0000 <akihisa.fromm1@gmail.com>
2022-04-09 15:44:45 +0900

    Updated

--- tekton-pipeline/github/setup.sh
+++ tekton-pipeline/github/setup.sh
@@ -1,6 +1,10 @@
 export GITHUB_USER  = ""
 export GITHUB_TOKEN = ""
 
+# Craete secret for GitHub Token
 sed -e "s;___GITHUB_USER___;${GITHUB__USER};g" \
 -e "s;___GITHUB_TOKEN___;${GITHUB__TOKEN};g"  \
 ./github-token-secret.yaml | kubectl create -f -
+
+# Create service account for the token
+kubectl apply -f ./tekton-admin.yaml
